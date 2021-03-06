df68041039709892633ed0ef504782c60e49c09f
ALXSMORA\Alexis Mora <alexis.mora1@icloud.com>
2021-03-06 17:39:08 -0600

    Update the logic and styles
    Add the values for the Props and the styled component for the tsx
    section.
    Import the AnswerObject and styled components.

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,37 +1,45 @@
 import React from 'react';
+//Types
+import { AnswerObject } from "../App";
+//Styles
+import { Wrapper, ButtonWrapper } from './QuestionCard.styles';
 
 type Props = {
     question: string;
     answer: string[];
-    callback: any;
-    userAnswer: any;
+    callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    userAnswer: AnswerObject | undefined;
     questionNr: number;
     totalQuestions: number;
 }
 
-const QuestionCard: React.FC<Props> = ({ 
+const QuestionCard: React.FC<Props> = ({
 question,
-answer, 
+answer,
 callback,
 userAnswer,
 questionNr,
 totalQuestions
 }) => (
-<div>
+<Wrapper>
     <p className="nnumber">
     Question: {questionNr} / {totalQuestions}
     </p>
-    <p dangerouslySetInnerHTML={{ __html: question }} />
+    <p className="question" dangerouslySetInnerHTML={{ __html: question }} />
     <div>
     {answer.map(answer => (
-    <div>
-        <button disabled={userAnswer} onClick={callback}>
+    <ButtonWrapper
+    key={answer}
+    correct={userAnswer?.correctAnswer === answer}
+    userClicked={userAnswer?.answer === answer}
+    >
+        <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
         <span dangerouslySetInnerHTML={{ __html: answer }} />
         </button>
-    </div>
+    </ButtonWrapper>
     ))}
     </div>
-</div>
+</Wrapper>
 );
 
 export default QuestionCard;
